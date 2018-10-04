16eab89f6db5be7087e1547392fc6911b62bc685
Dimitriy14 <yankovskiydy98@gmail.com>
2018-10-04 18:51:50 +0300

    MergeSort 4.10

---
+++ DimaTasks/mergeSort.go
@@ -0,0 +1,64 @@
+package main
+
+import "fmt"
+
+//This is mergesort algortim which uses to sort slices
+//Used src: https://en.wikipedia.org/wiki/Merge_sort
+
+func mergeSort(slice []int) []int {
+
+	if len(slice) <= 1 {
+		return slice
+	}
+
+	left := make([]int, 0)
+	right := make([]int, 0)
+	mid := len(slice) / 2
+
+	for i, x := range slice {
+		switch {
+		case i < mid:
+			left = append(left, x)
+		case i >= mid:
+			right = append(right, x)
+		}
+	}
+
+	left = mergeSort(left)
+	right = mergeSort(right)
+
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+
+	results := make([]int, 0)
+
+	for len(left) > 0 || len(right) > 0 {
+		if len(left) > 0 && len(right) > 0 {
+			if left[0] <= right[0] {
+				results = append(results, left[0])
+				left = left[1:len(left)]
+			} else {
+				results = append(results, right[0])
+				right = right[1:len(right)]
+			}
+		} else if len(left) > 0 {
+			results = append(results, left[0])
+			left = left[1:len(left)]
+		} else if len(right) > 0 {
+			results = append(results, right[0])
+			right = right[1:len(right)]
+		}
+	}
+
+	return results
+}
+
+func main() {
+
+	sl := []int{1, 6, -2, 7, 2, 99, 23, 311, 7, -8}
+	fmt.Println("Unsorted slice: ", sl)
+	sl = mergeSort(sl)
+	fmt.Println("Sorted slice: ", sl)
+}
